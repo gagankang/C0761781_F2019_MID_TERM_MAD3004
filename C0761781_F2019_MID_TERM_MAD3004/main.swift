19cdcac7e5e02c8b8e8feb1b8f3c3371fd21ec2f
Gaganpreet kaur <preetkang4511@gmail.com>
2019-10-19 19:36:47 -0400

    creating object

--- C0761781_F2019_MID_TERM_MAD3004/main.swift
+++ C0761781_F2019_MID_TERM_MAD3004/main.swift
@@ -8,5 +8,8 @@
 
 import Foundation
 
-print("Hello, World!")
+var mainDict = Dictionary<String, Customer>()
+
+var mobile1 = Mobile(manufacturerName: "Samsung", planName: "Rogers", mobileNo: "6478196786", gbUsed: 2.5, minUsed: 326, billID: "1", billDate: Date(), billType: Bill.bType.Mobile, totalBillAmount: 200)
+var internet1 = Internet(providerName: "Bell", internetGbUsed: 10.3, billID: "1", billDate: Date(), billType: <#T##Bill.bType#>, totalBillAmount: <#T##Float#>)
 
