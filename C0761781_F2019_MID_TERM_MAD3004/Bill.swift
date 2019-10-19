d51129a406d8dfd774828e9814d05eb7cbeb3196
Gaganpreet kaur <preetkang4511@gmail.com>
2019-10-19 18:04:05 -0400

    class bill Created

--- C0761781_F2019_MID_TERM_MAD3004/Bill.swift
+++ C0761781_F2019_MID_TERM_MAD3004/Bill.swift
@@ -7,3 +7,39 @@
 //
 
 import Foundation
+class Bill: IDisplay
+{
+    var billID: String
+    var billDate: Date
+    enum bType
+    {
+        case Internet, Mobile, Hydro
+    }
+    var billType: bType
+    var totalBillAmount: Float
+    init(billID: String, billDate: Date, billType: bType, totalBillAmount: Float)
+    {
+        self.billID = billID
+        self.billDate = billDate
+        self.billType = billType
+        self.totalBillAmount = totalBillAmount
+    }
+    
+    func  display()
+    {
+        print("Bill ID: \(billID)")
+        print("Bill Date: \(billDate)")
+        print("Bill Type: \(billType)")
+        print("Total Bill Amount: \(totalBillAmount)")
+        
+    }
+    }
+
+    
+    
+    
+    
+    
+    
+
+
