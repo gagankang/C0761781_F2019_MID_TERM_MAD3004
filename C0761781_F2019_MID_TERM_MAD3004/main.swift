b13ca8e52e89f809931087ccdcdf5054568d32c7
Gaganpreet kaur <preetkang4511@gmail.com>
2019-10-19 19:40:53 -0400

    creating customer object

--- C0761781_F2019_MID_TERM_MAD3004/main.swift
+++ C0761781_F2019_MID_TERM_MAD3004/main.swift
@@ -11,5 +11,11 @@ import Foundation
 var mainDict = Dictionary<String, Customer>()
 
 var mobile1 = Mobile(manufacturerName: "Samsung", planName: "Rogers", mobileNo: "6478196786", gbUsed: 2.5, minUsed: 326, billID: "1", billDate: Date(), billType: Bill.bType.Mobile, totalBillAmount: 200)
-var internet1 = Internet(providerName: "Bell", internetGbUsed: 10.3, billID: "1", billDate: Date(), billType: <#T##Bill.bType#>, totalBillAmount: <#T##Float#>)
+
+var internet1 = Internet(providerName: "Bell", internetGbUsed: 10.3, billID: "2", billDate: Date(), billType: Bill.bType.Internet, totalBillAmount: 57.96)
+
+var hydro1 = Hydro(agencyName: "Algoma", unitsConsumed: 233, billID: "41", billDate: Date(), billType: Bill.bType.Hydro, totalBillAmount: 97)
+
+var customer1 = Customer(customerID: "C01", firstName: "Gagan", lastName: "Kang", fullName: "Gagan Kang", emailID: "<#T##String#>", customerdict: <#T##Dictionary<String, Bill>#>, totalAmount: <#T##Float#>)
+
 
