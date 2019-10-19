216a977729f3c4e152d68f2853ddebc276fa6488
Gaganpreet kaur <preetkang4511@gmail.com>
2019-10-19 18:12:24 -0400

    declaration in mobile class

--- C0761781_F2019_MID_TERM_MAD3004/Mobile.swift
+++ C0761781_F2019_MID_TERM_MAD3004/Mobile.swift
@@ -9,9 +9,13 @@
 import Foundation
 class Mobile: Bill
 {
-    var manufacturerName: String
-    var planName
-    
-    
+    var manufacturerName: String!
+    var planName: String
+    var mobileNO: String
+    var gbUsed: Float
+    var minUsed: Float
+    init(manufacturerName: String, planName: String, mobileNo: String, gbUsed: Float, minUsed: Float, ) {
+        
+    }
 }
 
