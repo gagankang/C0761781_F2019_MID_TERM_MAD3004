050e002b09bab61d3ca042905dba9cde2c2edce7
Gaganpreet kaur <preetkang4511@gmail.com>
2019-10-19 19:06:37 -0400

    Customer class

---
+++ C0761781_F2019_MID_TERM_MAD3004/Customer.swift
@@ -0,0 +1,10 @@
+//
+//  Customer.swift
+//  C0761781_F2019_MID_TERM_MAD3004
+//
+//  Created by MacStudent on 2019-10-19.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+class Customer: IDisplay
